877fa154851369067684eb54166095419f9ddefe
Pratyush Shah <89347300+pratyushvshah@users.noreply.github.com>
2022-07-13 22:08:58 -0400

    Update log.sql

--- log.sql
+++ log.sql
@@ -4,14 +4,14 @@ CREATE TABLE users(
     Email TEXT NOT NULL,
     Username TEXT NOT NULL,
     Password TEXT NOT NULL,
-    Salt TEXT NOT NULL,
+    Salt TEXT NOT NULL
 );
 
 CREATE TABLE chatfriends(
     Username TEXT NOT NULL,
     Friends TEXT DEFAULT NULL,
     Requests TEXT DEFAULT NULL
-)
+);
 
 CREATE TABLE chatmessages(
     ID  SERIAL PRIMARY KEY,
@@ -26,4 +26,4 @@ CREATE TABLE chatmessages(
 CREATE TABLE chatsettings(
     Username TEXT NOT NULL,
     NumMessages INT DEFAULT 10
-)
+);
